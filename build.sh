0e4e0db1087d9f28c73aa1ca83ff4e355582238d
Ira <ira@ira.nz>
2024-06-02 01:54:49 +1200

    set yarn to berry so dlx command works

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set yarn to new version so dlx works
+
+yarn set version berry  
+
 # Build the Medusa server image
  if [ ! -d "medusa" ]; then
     yarn dlx @medusajs/medusa-cli@1.3.22 new --useDefaults medusa --skip-db
